cd45626df21c378decedb5af6534cbccd44c8789
devWeb-JP18 <jpkung16@outlook.com>
2023-06-03 16:28:54 -0300

    inicio pagina devs e quiz

--- js/quiz.js
+++ js/quiz.js
@@ -1,106 +1,68 @@
-const questions = [
-  {
-      question: "001",
-      answers:
-      [
-          { text: "A" , correct: true},
-          { text: "B" , correct: false},
-          { text: "C" , correct: false},
-          { text: "D" , correct: false},
-      ]
-  },
-  {
-      question: "Qual e o maior animal do mundo?",
-      answers:
-      [
-          { text: "Elefante" , correct: false},
-          { text: "Tubarao" , correct: false},
-          { text: "Baleia Azul" , correct: true},
-          { text: "Giraffa" , correct: false},
-      ]
-  },
-  {
-      question: "Qual e o maior animal do mundo?",
-      answers:
-      [
-          { text: "Elefante" , correct: false},
-          { text: "Tubarao" , correct: false},
-          { text: "Baleia Azul" , correct: true},
-          { text: "Giraffa" , correct: false},
-      ]
-  },
-  {
-      question: "Qual e o maior animal do mundo?",
-      answers:
-      [
-          { text: "Elefante" , correct: false},
-          { text: "Tubarao" , correct: false},
-          { text: "Baleia Azul" , correct: false},
-          { text: "Giraffa" , correct: true},
-      ]
-  },
-  {
-      question: "Qual e o maior animal do mundo?",
-      answers:
-      [
-          { text: "Elefante" , correct: false},
-          { text: "Tubarao" , correct: false},
-          { text: "Baleia Azul" , correct: true},
-          { text: "Giraffa" , correct: false},
-      ]
-  },
-  {
-      question: "Qual e o maior animal do mundo?",
-      answers:
-      [
-          { text: "Elefante" , correct: true},
-          { text: "Tubarao" , correct: false},
-          { text: "Baleia Azul" , correct: false},
-          { text: "Giraffa" , correct: false},
-      ]
-  },
-  {
-      question: "Qual e o maior animal do mundo?",
-      answers:
-      [
-          { text: "Elefante" , correct: false},
-          { text: "Tubarao" , correct: true},
-          { text: "Baleia Azul" , correct: false},
-          { text: "Giraffa" , correct: false},
-      ]
-  },
-  {
-      question: "Qual e o maior animal do mundo?",
-      answers:
-      [
-          { text: "Elefante" , correct: false},
-          { text: "Tubarao" , correct: false},
-          { text: "Baleia Azul" , correct: false},
-          { text: "Giraffa" , correct: true},
-      ]
-  },
-  {
-      question: "Qual e o maior animal do mundo?",
-      answers:
-      [
-          { text: "Elefante" , correct: true},
-          { text: "Tubarao" , correct: false},
-          { text: "Baleia Azul" , correct: false},
-          { text: "Giraffa" , correct: false},
-      ]
-  },
-  {
-      question: "Qual e o maior animal do mundo?",
-      answers:
-      [
-          { text: "Elefante" , correct: false},
-          { text: "Tubarao" , correct: true},
-          { text: "Baleia Azul" , correct: false},
-          { text: "Giraffa" , correct: false},
-      ]
-  },
-  
-  
+const questions = [{
+        question: "001",
+        answers: [
+            { text: "A", correct: true },
+            { text: "B", correct: false },
+            { text: "C", correct: false },
+            { text: "D", correct: false },
+        ]
+    },
+    {
+        question: "Qual e o maior animal do mundo?",
+        answers: [
+            { text: "Elefante", correct: false },
+            { text: "Tubarao", correct: false },
+            { text: "Baleia Azul", correct: true },
+            { text: "Giraffa", correct: false },
+        ]
+    },
+    {
+        question: "Qual e o maior animal do mundo?",
+        answers: [
+            { text: "Elefante", correct: false },
+            { text: "Tubarao", correct: false },
+            { text: "Baleia Azul", correct: true },
+            { text: "Giraffa", correct: false },
+        ]
+    },
+    {
+        question: "Qual e o maior animal do mundo?",
+        answers: [
+            { text: "Elefante", correct: false },
+            { text: "Tubarao", correct: false },
+            { text: "Baleia Azul", correct: false },
+            { text: "Giraffa", correct: true },
+        ]
+    },
+    {
+        question: "Qual e o maior animal do mundo?",
+        answers: [
+            { text: "Elefante", correct: false },
+            { text: "Tubarao", correct: false },
+            { text: "Baleia Azul", correct: true },
+            { text: "Giraffa", correct: false },
+        ]
+    },
+    {
+        question: "Qual e o maior animal do mundo?",
+        answers: [
+            { text: "Elefante", correct: true },
+            { text: "Tubarao", correct: false },
+            { text: "Baleia Azul", correct: false },
+            { text: "Giraffa", correct: false },
+        ]
+    },
+    {
+        question: "Qual e o maior animal do mundo?",
+        answers: [
+            { text: "Elefante", correct: false },
+            { text: "Tubarao", correct: true },
+            { text: "Baleia Azul", correct: false },
+            { text: "Giraffa", correct: false },
+        ]
+    }
+
+
 ];
 
 
@@ -108,85 +70,85 @@ const questions = [
 
 
 const questionElement = document.getElementById("question");
-const  answerButtons = document.getElementById("answer-buttons");
-const  nextButton= document.getElementById("next-botn");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-botn");
 
 let currentQuestionIndex = 0;
 let Score = 0;
 
-function startQuiz () {
-  currentQuestionIndex = 0;
-  Score = 0;
-  nextButton.innerHTML = "Próxima";
-  showQuestion ();
+function startQuiz() {
+    currentQuestionIndex = 0;
+    Score = 0;
+    nextButton.innerHTML = "Próxima";
+    showQuestion();
 }
 
 
-function showQuestion () {
-resetState ();
-  let currentQuestion = questions [currentQuestionIndex];
-  let questionNo = currentQuestionIndex + 1;
-  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
-  currentQuestion.answers.forEach(answers => {
-      const button = document.createElement ("button");
-      button.innerHTML = answers.text;
-      button.classList.add("botn");
-      answerButtons.appendChild(button);
-      if(answers.correct){
-          button.dataset.correct = answers.correct;
-      }
-      button.addEventListener("click", selectAnswer);
-  });
+    currentQuestion.answers.forEach(answers => {
+        const button = document.createElement("button");
+        button.innerHTML = answers.text;
+        button.classList.add("botn");
+        answerButtons.appendChild(button);
+        if (answers.correct) {
+            button.dataset.correct = answers.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
 }
 
 
-function resetState(){
-  nextButton.style.display = "none";
-  while(answerButtons.firstChild) {
-      answerButtons.removeChild(answerButtons.firstChild);
-  }
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
 }
 
-function selectAnswer(e){
-  const selectedBtn = e.target;
-  const isCorrect = selectedBtn.dataset.correct === "true";
-  if (isCorrect){
-      selectedBtn.classList.add("correct");
-      Score++;
-  }else{
-      selectedBtn.classList.add("incorrect");
-  }
-  Array.from(answerButtons.children).forEach(button => {
-      if(button.dataset.correct === "true"){
-          button.classList.add("correct");
-      }
-      button.disabled = true;
-  });
-  nextButton.style.display = "block";
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        Score++;
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
 }
 
-function showScore(){
-  resetState();
-  questionElement.innerHTML = `Você acertou ${Score} de ${questions.length} perguntas!`;
-  nextButton.innerHTML = "Jogar novamente";
-  nextButton.style.display = "block";
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `Você acertou ${Score} de ${questions.length} perguntas!`;
+    nextButton.innerHTML = "Jogar novamente";
+    nextButton.style.display = "block";
 }
 
-function handleNextButton(){
-  currentQuestionIndex++;
-  if(currentQuestionIndex < questions.length){
-      showQuestion();
-  }else{
-      showScore();
-  }
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
 }
 
-nextButton.addEventListener("click", ()=>{
-  if(currentQuestionIndex < questions.length){
-      handleNextButton();
-  }else{
-      startQuiz();
-  }
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
 })
-startQuiz ();
+startQuiz();
